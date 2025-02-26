a1e31195a865461428ba683797b8a9ce92366e1f
Rafaela <rafaelaperes215@gmail.com>
2025-02-26 20:45:49 -0300

    Feat: Adicionado página de login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import LoginForm from './LoginForm'
-import LoginCreate from './LoginCreate'
-import LoginPasswordLost from './LoginPasswordLost'
-import LoginPasswordReset from './LoginPasswordReset'
+import React from "react";
+import styles from "./Login.module.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import LoginCreate from "./LoginCreate";
+import LoginPasswordLost from "./LoginPasswordLost";
+import LoginPasswordReset from "./LoginPasswordReset";
+import { UserContext } from "../../UserContext";
 
 const Login = () => {
-  return (
-    <div>
-      <Routes>
-        <Route path='/' element={<LoginForm />} />
-        <Route path='criar' element={<LoginCreate />} />
-        <Route path='perdeu' element={<LoginPasswordLost />} />
-        <Route path='resetar' element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
-  )
-}
+  const { login } = React.useContext(UserContext);
 
-export default Login
+  if (!login) {
+    return (
+      <section className={styles.login}>
+        <div className={styles.forms}>
+          <Routes>
+            <Route path="/" element={<LoginForm />} />
+            <Route path="cadastrarLogin" element={<LoginCreate />} />
+            <Route path="esqueceuSenha" element={<LoginPasswordLost />} />
+            <Route path="resetar" element={<LoginPasswordReset />} />
+          </Routes>
+        </div>
+      </section>
+    );
+  } else {
+    return <Navigate to="/conta" />;
+  }
+};
+
+export default Login;
