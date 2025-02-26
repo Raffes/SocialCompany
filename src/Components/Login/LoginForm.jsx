ccedba6049b40984d68710a2d5ab08a98a016e6c
Rafaela <rafaelaperes215@gmail.com>
2025-02-26 20:47:08 -0300

    Feat: Adicionado página de formulário de login

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,45 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styles from "./LoginForm.module.css";
+import stylesBtn from '../Forms/Button.module.css'
+import { Link } from "react-router-dom";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
+import { UserContext } from "../../UserContext";
+import Error from "../Helper/Error";
 
 const LoginForm = () => {
-    const [username, setUsername] = React.useState('');
-    const [password, setPassword] = React.useState('');
+  const username = useForm();
+  const password = useForm();
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({username, password}),
-        }).then(response => {
-            console.log(response);
-            return response.json();
-        }).then((json) => {
-            console.log(json);
-        })
+  const { userLogin, error, loading } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (username.validate() && password.validate()) {
+      userLogin(username.value, password.value);
     }
+  }
 
   return (
-    <section>
-        <h1>Login</h1>
-        <form action="" onSubmit={handleSubmit}>
-            <input 
-                type="text" 
-                value={username}
-                onChange={({target}) => setUsername(target.value)} />
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" {...username} />
 
-            <input 
-                type="text" 
-                value={password}
-                onChange={({target}) => setPassword(target.value)} />
+        <Input label="Senha" type="password" {...password} />
 
-            <button>Entrar</button>
-        </form>
-      <Link to="/login/criar">Cadastro</Link>
+        {loading ? (
+          <Button disabled label="Carregando..." />
+        ) : (
+          <Button label="Entrar" />
+        )}
+
+        <Error error={error} />
+      </form>
+      <Link className={styles.esqueceuSenha} to="/login/esqueceuSenha">Esqueceu a senha?</Link>
+      
+      <div className={styles.cadastroLogin}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className={stylesBtn.button} to="/login/cadastrarLogin">Cadastro</Link>
+      </div>
+    
     </section>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
