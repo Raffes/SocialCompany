a62a763debf63a877cd02092ac1ca9030e14c469
Rafaela <rafaelaperes215@gmail.com>
2025-02-26 20:50:08 -0300

    Feat: Adicionado menu da página

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,15 +2,30 @@ import React from 'react'
 import styles from './Header.module.css'
 import { Link } from 'react-router-dom'
 import SocialCompany from '../Assets/dogs.svg?react'
+import { UserContext } from '../UserContext'
 
 const Header = () => {
+  const { data, userLogout } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link className={styles.logo} to="/" aria-label='Social Company - Home'>
           <SocialCompany />
         </Link>
-        <Link className={styles.login} to="/login">Login / Criar</Link>
+
+        { data ? (
+          <>
+            <Link className={styles.login} to="/conta">
+              {data.nome}
+            </Link>
+            <button onClick={userLogout}>Sair</button>
+          </>
+        ): (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   )
