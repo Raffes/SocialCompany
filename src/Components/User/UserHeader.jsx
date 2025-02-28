b3616e12e93c3d9274b9dea17d6fbc00ab478d6e
Rafaela <rafaelaperes215@gmail.com>
2025-02-27 21:58:25 -0300

    Feat: Adicionado header do usuário

---
+++ src/Components/User/UserHeader.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import UserHeaderNav from './UserHeaderNav'
+import styles from './UserHeader.module.css'
+import { useLocation } from 'react-router-dom';
+
+const UserHeader = () => {
+  const [title, setTitle] = React.useState('');
+  const location = useLocation();
+
+  React.useEffect(() => {
+    switch(location.pathname) {
+      case '/conta':
+        return setTitle('Minhas Fotos');
+      case '/conta/estatisticas':
+        return setTitle('Estatísticas');
+      case '/conta/criarPost':
+        return setTitle('Criar Post');
+    }
+  }, [location]);
+
+  return (
+    <header className={styles.header}>
+      <h1 className='title'>{title}</h1>
+      <UserHeaderNav />
+    </header>
+  )
+}
+
+export default UserHeader
