6c25be0649a3131c8fc186456cf330538d7f0677
Rafaela <rafaelaperes215@gmail.com>
2025-03-05 16:26:55 -0300

    Feat: Adicionado página de esquece senha

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import Error from "../Helper/Error";
+import useForm from "../../Hooks/useForm";
+import { PASSWORD_LOST } from "../../api";
+import useFetch from "../../Hooks/useFetch";
 
 const LoginPasswordLost = () => {
-  return (
-    <div>
-      LoginPasswordLost
-    </div>
-  )
-}
+  const login = useForm("email");
 
-export default LoginPasswordLost
+  const { data, loading, error, request } = useFetch();
+
+  async function handleLostPassword(event) {
+    event.preventDefault();
+    if (login.validate()) {
+      const { url, options } = PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace('perdeu', 'resetar')
+      });
+      await request(url, options);
+    }
+  }
+
+  return (
+    <section>
+      <h1 className="title">Esqueceu a senha?</h1>
+      {data ? <p style={{color: '#4c1'}}>{data}</p> : (
+        <form onSubmit={handleLostPassword}>
+        <Input label="Email / Usuário" {...login} />
+
+        {loading ? (
+          <Button label="Enviando..." disabled />
+        ) : (
+          <Button label="Enviar email" />
+        )}
+
+        <Error error={error} />
+      </form>
+      )}
+    </section>
+  );
+};
+
+export default LoginPasswordLost;
