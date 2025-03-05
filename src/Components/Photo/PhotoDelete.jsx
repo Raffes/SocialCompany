11342939ff842e90ce530e580337d2930dd874e5
Rafaela <rafaelaperes215@gmail.com>
2025-03-05 09:05:46 -0300

    Feat: Adicionado função de deletar foto

---
+++ src/Components/Photo/PhotoDelete.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./PhotoDelete.module.css";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTO_DELETE } from "../../api";
+
+const PhotoDelete = ({ id }) => {
+  const { request, loading } = useFetch();
+
+  async function handleDeletePhoto() {
+    const confirm = window.confirm("Tem certeza de que deseja deletar a foto?");
+    if (confirm) {
+      const token = window.localStorage.getItem("token");
+      const { url, options } = PHOTO_DELETE(id, token);
+      const { response } = await request(url, options);
+      if (response.ok) {
+        window.location.reload();
+      }
+    }
+  }
+
+  return (
+    <>
+      {loading ? (
+        <button className={styles.delete} disabled>
+          Deletar
+        </button>
+      ) : (
+        <button onClick={handleDeletePhoto} className={styles.delete}>
+          Deletar
+        </button>
+      )}
+    </>
+  );
+};
+
+export default PhotoDelete;
