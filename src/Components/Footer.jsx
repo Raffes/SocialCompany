1a7f9d7bf88f144986ee8e02fcd8f2d48b10546e
Rafaela <rafaelaperes215@gmail.com>
2025-03-05 20:45:02 -0300

    Feat: Ajustado para o footer ficar sempre no final da página

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import SocialCompanySVG from '../Assets/dogs-footer.svg?react'
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <div className={styles.footer}>
-      
+      <SocialCompanySVG />
+      <p>&copy; {year} | Social Company </p>
     </div>
   )
 }
