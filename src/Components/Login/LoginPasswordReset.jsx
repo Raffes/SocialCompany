32aa290bd4a4377574c745bcaaf18a8072a7ef47
Rafaela <rafaelaperes215@gmail.com>
2025-03-05 16:56:26 -0300

    Feat: Adicionado página de reset de senha

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import Error from "../Helper/Error";
+import useForm from "../../Hooks/useForm";
+import { PASSWORD_RESET } from "../../api";
+import useFetch from "../../Hooks/useFetch";
+import { useNavigate } from "react-router-dom";
 
 const LoginPasswordReset = () => {
-  return (
-    <div>
-      LoginPasswordReset
-    </div>
-  )
-}
+  const [login, setLogin] = React.useState('');
+  const [key, setKey] = React.useState('');
+  const password = useForm("password");
+  const navigate = useNavigate();
 
-export default LoginPasswordReset
+  const { loading, error, request } = useFetch();
+
+  async function handleResetPassword(event) {
+    event.preventDefault();
+    if (password.validate()) {
+      const { url, options } = PASSWORD_RESET({
+        login,
+        key,
+        password: login.password,
+      });
+      const { response } = await request(url, options);
+
+      if(response.ok) navigate('/login');
+    }
+  }
+
+  React.useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const key = params.get('key');
+    const login = params.get('login');
+    if(key) setKey(key);
+    if(login) setLogin(login);
+  }, []);
+
+  return (
+    <section>
+      <h1 className="title">Resetar senha</h1>
+      <form onSubmit={handleResetPassword}>
+        <Input label="Nova senha" type={'password'} {...password} />
+
+        {loading ? (
+          <Button label="Resetando..." disabled />
+        ) : (
+          <Button label="Resetar" />
+        )}
+
+        <Error error={error} />
+      </form>
+    </section>
+  );
+};
+
+export default LoginPasswordReset;
