f1617432e3a9b6a42fe78784eb00f8894a2e87c3
Rafaela <rafaelaperes215@gmail.com>
2025-03-06 15:15:09 -0300

    Fix: Ajustado feedback

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -37,7 +37,7 @@ const LoginForm = () => {
           <Button label="Entrar" />
         )}
 
-        <Error error={error} />
+        <Error error={error && 'Dados incorretos.'} />
       </form>
       <Link className={styles.esqueceuSenha} to="/login/esqueceuSenha">Esqueceu a senha?</Link>
       
