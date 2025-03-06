c616f1fd09696ea04097096d2dbe63f5034fdad7
Rafaela <rafaelaperes215@gmail.com>
2025-03-05 21:50:28 -0300

    Feat: Adicionado biblioteca de graficos para ser adicionada na página estatísticas

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import Head from '../Helper/Head'
+import useFetch from '../../Hooks/useFetch';
+import { GET_STATS } from '../../api';
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      UserStats
-    </div>
-  )
+  const { data, error, loading, request } = useFetch();
+  
+  React.useEffect(() => {
+    async function getData() {
+      const token = window.localStorage.getItem("token");
+      const { url, options } = GET_STATS(token);
+      await request(url, options);
+    }
+    getData();
+  }, [request])
+
+  if(loading) return <Loading />
+  if(error) return <Error error={error} />
+  if(data) {
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    )
+  } else return null;
 }
 
 export default UserStats
